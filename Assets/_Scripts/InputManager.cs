d809b437f0dee3cc19ab25aa753ec2e8544b0093
rinwindows <phurinmeng@gmail.com>
2025-03-10 21:32:38 -0700

    Implement settings menu toggle and quit functionality in GameManager; add OnSettingsMenu event in InputManager

--- Assets/_Scripts/InputManager.cs
+++ Assets/_Scripts/InputManager.cs
@@ -6,6 +6,7 @@ public class InputManager : MonoBehaviour
     public UnityEvent<Vector2> OnMove = new();
     public UnityEvent OnJump = new();
     public UnityEvent OnDash = new();
+    public UnityEvent OnSettingsMenu = new();
     void Update()
     {
         Vector2 input = Vector2.zero;
@@ -35,5 +36,10 @@ public class InputManager : MonoBehaviour
             OnDash?.Invoke();
         }
         OnMove?.Invoke(input.normalized);
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            OnSettingsMenu?.Invoke();
+        }
     }
 }
